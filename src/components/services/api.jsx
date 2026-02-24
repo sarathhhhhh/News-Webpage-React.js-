292595454f1d2ace1620f2b20a97946ec0d35235
sarathhhhhh <sarathsastham@gmail.com>
2026-02-24 16:13:02 +0530

    final api root change

--- src/components/services/api.jsx
+++ src/components/services/api.jsx
@@ -1,11 +1,23 @@
 import axios from "axios";
 
-export const fetchNews = async () => {
-  try {
-    const response = await axios.get("/api/news");
-    return response.data.articles;
-  } catch (error) {
-    console.log("API Error:", error);
-    return [];
-  }
+const API_KEY='93210ef0891cb87bb9e8c2df09943f12';
+
+export const fetchNews=async(category="general")=>{
+    try{
+         const response = await axios.get(
+
+        "https://gnews.io/api/v4/top-headlines",
+      {
+        params: {
+          country: "us",
+          category: category,
+          token: API_KEY,
+          max: 9,
+                },
+            }
+        );
+        return response.data.articles;
+    } catch(error){
+        console.log('API Error:',error);
+    }
 };
